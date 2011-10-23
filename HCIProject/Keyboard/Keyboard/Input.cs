452a78fa90fc2ca113c0ad9cfa61a238c64842eb
seph.newman@gmail.com <seph.newman@gmail.com@f2b6003c-1667-afc0-eb42-f1e0dd1bada9>
2011-10-23 23:09:44 +0000

    -Initial check in with code base. (for real this time) -A test game allows us to pull up either are keyboard or the default keyboard.

---
+++ HCIProject/Keyboard/Keyboard/Input.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Keyboard
+{
+
+    public class Input
+    {
+        PlayerIndex player;
+
+
+        public float lx;
+        public float ly;
+        public float rx;
+        public float ry;
+
+        public float rt;
+        public float lt;
+
+        public bool a;
+        public bool b;
+        public bool x;
+        public bool y;
+
+        public bool rb;
+        public bool lb;
+
+        public bool start;
+        public bool back;
+
+        public bool dup;
+        public bool dright;
+        public bool ddown;
+        public bool dleft;
+
+        public Input()
+        {
+            a = false;
+            b = false;
+            x = false;
+            y = false;
+
+            rb = false;
+            lb = false;
+
+            start = false;
+            back = false;
+
+            dup = false;
+            dright = false;
+            ddown = false;
+            dleft = false;
+
+            lx = 0;
+            ly = 0;
+
+            rx = 0;
+            ry = 0;
+
+            rt = 0;
+            lt = 0;
+        }
+
+        public Input(PlayerIndex playerGiven)
+        {
+            player = playerGiven;
+
+            a = false;
+            b = false;
+            x = false;
+            y = false;
+
+            rb = false;
+            lb = false;
+
+            start = false;
+            back = false;
+
+            dup = false;
+            dright = false;
+            ddown = false;
+            dleft = false;
+
+            lx = 0;
+            ly = 0;
+
+            rx = 0;
+            ry = 0;
+
+            rt = 0;
+            lt = 0;
+        }
+
+        public Input Copy()
+        {
+            Input newInput = new Input(player);
+            newInput.a = a;
+            newInput.b = b;
+            newInput.x = x;
+            newInput.y = y;
+            newInput.start = start;
+            newInput.back = back;
+            newInput.lb = lb;
+            newInput.rb = rb;
+            newInput.lx = lx;
+            newInput.ly = ly;
+            newInput.rx = rx;
+            newInput.ry = ry;
+            newInput.ddown = ddown;
+            newInput.dup = dup;
+            newInput.dleft = dleft;
+            newInput.dright = dright;
+
+            return newInput;
+        }
+
+        public void Update()
+        {
+            GamePadState currentState = GamePad.GetState(player);
+
+            if (currentState.Buttons.A == ButtonState.Pressed)
+                a = true;
+            else
+                a = false;
+
+            if (currentState.Buttons.B == ButtonState.Pressed)
+                b = true;
+            else
+                b = false;
+
+            if (currentState.Buttons.X == ButtonState.Pressed)
+                x = true;
+            else
+                x = false;
+
+            if (currentState.Buttons.Y == ButtonState.Pressed)
+                y = true;
+            else
+                y = false;
+
+            if (currentState.Buttons.LeftShoulder == ButtonState.Pressed)
+                lb = true;
+            else
+                lb = false;
+
+            if (currentState.Buttons.RightShoulder == ButtonState.Pressed)
+                rb = true;
+            else
+                rb = false;
+
+            if (currentState.DPad.Up == ButtonState.Pressed)
+                dup = true;
+            else
+                dup = false;
+
+            if (currentState.DPad.Left == ButtonState.Pressed)
+                dleft = true;
+            else
+                dleft = false;
+
+            if (currentState.DPad.Right == ButtonState.Pressed)
+                dright = true;
+            else
+                dright = false;
+
+            if (currentState.DPad.Down == ButtonState.Pressed)
+                ddown = true;
+            else
+                ddown = false;
+
+            if (currentState.Buttons.Start == ButtonState.Pressed)
+                start = true;
+            else
+                start = false;
+
+            if (currentState.Buttons.Back == ButtonState.Pressed)
+                back = true;
+            else
+                back = false;
+
+            lx = currentState.ThumbSticks.Left.X;
+            ly = currentState.ThumbSticks.Left.Y;
+
+            rx = currentState.ThumbSticks.Right.X;
+            ry = currentState.ThumbSticks.Right.Y;
+
+
+        }
+
+
+
+
+    }
+}
---
+++ HCIProject/Keyboard/Keyboard/Keyboard.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Keyboard
+{
+    public enum KeyboardStatus { Entering, Leaving, Active, Inactive }
+
+    public class Keyboard : RenderArea
+    {
+        KeyboardStatus CurrentStatus;
+
+        Color FadeColor;
+        Sprite Fade;
+        float FadeAlpha;
+        float FadeAlphaMax;
+        float FadeIncrement;
+
+        KeyboardFrame Frame;
+        int FrameOnScreenY;
+        int FrameOffScreenY;
+        int FrameVelocityY;
+
+        public bool IsActive { get { return CurrentStatus != KeyboardStatus.Inactive; } }
+
+        public Keyboard(KeyboardTextures textures, KeyboardColors colors)
+        {
+            //If we get a chance, we should modify this to work with other resolutions
+            InitializeRenderArea(0, 0, 1280, 720);
+
+            Fade = new Sprite(textures.Fade, new Vector2(0, 0));
+            FadeColor = colors.Fade;
+            FadeAlpha = 0;
+            FadeAlphaMax = 180;
+            FadeIncrement = 15;
+
+            FrameOnScreenY = 50;
+            FrameOffScreenY = 720;
+            FrameVelocityY = -5;
+
+            Frame = new KeyboardFrame(200, FrameOffScreenY, textures.Frame, colors.Frame);
+
+            
+
+            CurrentStatus = KeyboardStatus.Inactive;
+        }
+
+
+
+        public void Enter()
+        {
+            CurrentStatus = KeyboardStatus.Entering;
+        }
+
+        public void Leave()
+        {
+            CurrentStatus = KeyboardStatus.Leaving;
+        }
+
+
+        public void Update(GameTime gameTime)
+        {
+            
+
+            switch (CurrentStatus)
+            {
+                case KeyboardStatus.Entering:
+                    EnterUpdate(gameTime);
+                    break;
+                case KeyboardStatus.Leaving:
+                    LeaveUpdate(gameTime);
+                    break;
+                case KeyboardStatus.Active:
+                    break;
+            }
+
+            Frame.Update(gameTime);
+        }
+
+        public void LeaveUpdate(GameTime gameTime)
+        {
+            Frame.FramePosition.Y-= FrameVelocityY * ((float)gameTime.ElapsedGameTime.TotalMilliseconds);
+            FadeAlpha -= FadeIncrement;
+
+            bool MoveComplete = false;
+            bool FadeComplete = false;
+
+            if (Frame.FramePosition.Y >= FrameOffScreenY)
+            {
+                Frame.FramePosition.Y = FrameOffScreenY;
+                MoveComplete = true;
+            }
+
+            if (FadeAlpha <= 0)
+            {
+                FadeAlpha = 0;
+                FadeComplete = true;
+            }
+
+            if (MoveComplete && FadeComplete)
+            {
+                CurrentStatus = KeyboardStatus.Inactive;
+            }
+        }
+
+        public void EnterUpdate(GameTime gameTime)
+        {
+            Frame.FramePosition.Y +=(FrameVelocityY * ((float)gameTime.ElapsedGameTime.TotalMilliseconds));
+            FadeAlpha += FadeIncrement;
+
+            bool MoveComplete = false;
+            bool FadeComplete = false;
+
+            if (Frame.FramePosition.Y <= FrameOnScreenY)
+            {
+                Frame.FramePosition.Y =FrameOnScreenY;
+                MoveComplete = true;
+            }
+
+            if (FadeAlpha >= FadeAlphaMax)
+            {
+                FadeAlpha = FadeAlphaMax;
+                FadeComplete = true;
+            }
+
+            if (MoveComplete && FadeComplete)
+            {
+                CurrentStatus = KeyboardStatus.Active;
+            }
+        }
+
+        protected override void Draw(SpriteBatch spriteBatch)
+        {
+            Fade.Color = new Color(FadeHelper(FadeColor.R), FadeHelper(FadeColor.G), FadeHelper(FadeColor.B), FadeHelper(255));
+            Fade.Draw(spriteBatch);
+            Frame.Draw(spriteBatch);
+        }
+
+        //I don't think we will need this for our keyboard.  I will go back and edit RenderArea later
+        protected override void PostDraw(Texture2D screen, SpriteBatch spriteBatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte FadeHelper(int max)
+        {
+            return (byte)MathHelper.Clamp((FadeAlpha / 255) * max, 0, max);
+        }
+    }
+}
