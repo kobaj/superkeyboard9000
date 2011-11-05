94584e4fdfb5f342dad68ad9c364440b1eff727c
seph.newman@gmail.com <seph.newman@gmail.com@f2b6003c-1667-afc0-eb42-f1e0dd1bada9>
2011-11-05 22:28:21 +0000

    Forgot to check in a file.

---
+++ trunk/HCIProject/Keyboard/Keyboard/KeyboardFonts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Keyboard
+{
+    public class KeyboardFont
+    {
+        public SpriteFont Keys;
+        public SpriteFont InputText;
+        public SpriteFont Labels;
+    }
+}
